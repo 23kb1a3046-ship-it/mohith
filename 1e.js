95bee0c222808e684c2844ec7aa29eb590168129
k.mohith <23kb1a3046@nbkrist.org>
2025-09-15 14:31:31 +0530

    Add files via upload

---
+++ 1e.js
@@ -0,0 +1,12 @@
+function greet(name) {
+  return `Hello, ${name}!`;
+}
+console.log(greet("Alice"));
+const multiply = function(x, y) {
+  return x * y;
+};
+console.log(multiply(4, 5)); 
+const divide = (a, b) => {
+  return a / b;
+};
+console.log(divide(10, 2));
